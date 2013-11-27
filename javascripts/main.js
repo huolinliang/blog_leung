cdc1ec3e91602cf8aaf124cda2ad11bd850b1965
leung <huolin-leung@foxmail.com>
2013-11-27 14:13:14 +0800

    try to add a function for comment

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
 console.log('This would be the main JS file.');
+$('#disqus_container .comment').on('click',function(){
+$(this).html('加载中...');
+var disqus_shortname = 'blogofleung';
+var that = this;
+BYB.includeScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()}); //这是一个加载js的函数
+});
